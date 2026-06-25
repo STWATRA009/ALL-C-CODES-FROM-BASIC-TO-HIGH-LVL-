873a0c0758c1d3e1095511063f6b354beb7faa52
ẞtwátrà Krûßhñâ Máñgâràj <skmangaraj2006@gmail.com>
2026-06-25 22:47:22 +0530

    Add files via upload

---
+++ ALL CODES BASIC/CODE7.c
@@ -0,0 +1,15 @@
+//WAP TO CALCULATE THE AREA OF RECTANGLE. (BY USING TWO VARIABLES)
+#include<stdio.h>
+int main()
+{
+    int l,b;
+    /*
+    l = length 
+    b = breadth
+    */ 
+    printf("ENTER THE LENGTH AND BREADTH OF RECTANGLE\n");
+    scanf("%d %d",&l,&b);
+    printf("THE AREA OF RECTANGLE IS %d",l*b);
+
+    return 0;
+}
